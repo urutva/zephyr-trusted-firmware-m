ed0e07b68a1b66c06dd700a317f85437092efb18
Devaraj Ranganna <devaraj.ranganna@arm.com>
2021-09-23 12:26:44 +0100

    trusted-firmware-m: example partition: Add a new secure service
    A new secure service is added which reads LSM303DLHC magnetometer values
    using secure I2C3 driver.
    
    Currently, this secure service only works with MPS2 AN521 board.
    
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@linaro.org>

--- trusted-firmware-m/secure_fw/partitions/example_partition/tfm_example_partition_api.h
+++ trusted-firmware-m/secure_fw/partitions/example_partition/tfm_example_partition_api.h
@@ -31,7 +31,7 @@ extern "C" {
  *
  * \param[in]   input           Message to be hashed
  * \param[in]   input_length    Length of message in bytes
- * \param[out]  hash            Buffer to which calculated hash will be
+ * \param[out]  hash            Buffer to which calculated hash is
  *                              written into
  * \param[in]   hash_buf_size   Size of hash buffer in bytes
  * \param[out]  hash_len        Actual size of calculated hash in bytes
@@ -43,6 +43,18 @@ psa_status_t psa_example_hash(const uint8_t *input,
 				             uint8_t *hash,
                              size_t hash_buf_size,
 				             size_t *hash_len);
+
+/**
+ * \brief Read magnetometer (LSM303) data.
+ *
+ * \param[out]  data            Buffer to which magnetometer data is
+ *                              written into
+ * \param[out]   data_size      Size of magnetometer data in bytes
+ *
+ * \return Returns error code as specified in \ref psa_status_t
+ */
+psa_status_t example_read_lsm303(uint8_t *data,
+                                size_t data_size);
 
 #ifdef __cplusplus
 }
